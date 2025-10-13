9c1128deeb54eee0463ec033382a7fffdab78995
zhm <frontzhm@163.com>
2025-10-13 09:29:40 +0800

    feat: 阶段一初始化一段

--- src/index.js
+++ src/index.js
@@ -15,9 +15,16 @@ class MiniWebpack {
     this.emit();
     console.log('🎉 构建成功完成！');
   }
-  initialize() {}
-  compile() {}
-  emit() {}
+  // 阶段一：初始化阶段
+  initialize() {
+    console.log('🚀 阶段一：初始化阶段');
+    console.log('📖 读取配置文件:', this.config); // 其实就是拿到this.config,实际的时候应该是需要和命令行参数结合的
+    console.log('🔧 创建 Compiler 实例');
+    console.log('📝 解析 Entry 入口:', this.config.entry); // 其实就是拿到this.config.entry
+    console.log('✅ 初始化完成\n');
+  }
+  compile() { }
+  emit() { }
 }
 
 module.exports = MiniWebpack;
